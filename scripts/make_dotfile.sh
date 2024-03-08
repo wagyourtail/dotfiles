2f4c7823777d18ffa9677bf0da7d35cbd502e398
Wagyourtail <wagyourtail@wagyourtail.xyz>
2024-03-08 01:16:29 -0700

    update install

--- scripts/make_dotfile.sh
+++ scripts/make_dotfile.sh
@@ -9,8 +9,14 @@
 REALPATH=$(readlink -f "$1")
 RELPATH=$(realpath -s --relative-to "$HOME" "$REALPATH")
 
+# create in dotfiles
 mkdir -p "$(dirname "$HOME/dotfiles/$RELPATH")"
-cp "$1" "$HOME/dotfiles/$RELPATH"
+cp "$HOME/$RELPATH" "$HOME/dotfiles/$RELPATH"
+
+# write to install script
+echo "mv \"\$HOME/$RELPATH\" \"\$HOME/$RELPATH.bak\"" >> $HOME/dotfiles/scripts/install_dotfiles.sh
 echo "ln -s \"\$HOME/dotfiles/$RELPATH\" \"\$HOME/$RELPATH\"" >> $HOME/dotfiles/scripts/install_dotfiles.sh
-rm "$1"
+
+# run install script commands
+mv "$HOME/$RELPATH" "$HOME/$RELPATH.bak"
 ln -s "$HOME/dotfiles/$RELPATH" "$HOME/$RELPATH"
