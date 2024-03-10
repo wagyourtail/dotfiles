b6761b5e18ffd878bce2982b1c58f2435bf52cd5
Wagyourtail <wagyourtail@wagyourtail.xyz>
2024-03-09 19:15:21 -0700

    delay more things

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,3 +3,7 @@
 sleep 5
 hyprctl dispatch exec "steam -nochatui -nofriendsui -silent"
 hyprctl dispatch exec vesktop
+hyprctl dispatch exec "$HOME/dotfiles/scripts/wallpaper.py"
+
+# Set Primary display for xwayland applications (fixes cs2)
+xrandr --output DP-3 --primary
