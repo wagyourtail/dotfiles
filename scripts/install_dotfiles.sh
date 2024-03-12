cee1105e1f504a199fad94ef4bd0db6e5327238c
Wagyourtail <wagyourtail@wagyourtail.xyz>
2024-03-12 06:02:37 -0600

    refactor wallpaper.py

--- scripts/install_dotfiles.sh
+++ scripts/install_dotfiles.sh
@@ -9,19 +9,26 @@ mv "$HOME/.config/hypr/hyprlock.conf" "$HOME/.config/hypr/hyprlock.conf.bak"
 ln -s "$HOME/dotfiles/.config/hypr/hyprlock.conf" "$HOME/.config/hypr/hyprlock.conf"
 mv "$HOME/.config/hypr/hypridle.conf" "$HOME/.config/hypr/hypridle.conf.bak"
 ln -s "$HOME/dotfiles/.config/hypr/hypridle.conf" "$HOME/.config/hypr/hypridle.conf"
+
 mv "$HOME/.config/dunst/dunstrc" "$HOME/.config/dunst/dunstrc.bak"
 ln -s "$HOME/dotfiles/.config/dunst/dunstrc" "$HOME/.config/dunst/dunstrc"
+
 mv "$HOME/.config/wofi/style.css" "$HOME/.config/wofi/style.css.bak"
 ln -s "$HOME/dotfiles/.config/wofi/style.css" "$HOME/.config/wofi/style.css"
+
 mv "$HOME/.config/waybar/config" "$HOME/.config/waybar/config.bak"
 ln -s "$HOME/dotfiles/.config/waybar/config" "$HOME/.config/waybar/config"
 mv "$HOME/.config/waybar/style.css" "$HOME/.config/waybar/style.css.bak"
 ln -s "$HOME/dotfiles/.config/waybar/style.css" "$HOME/.config/waybar/style.css"
+
 mv "$HOME/.config/electron-flags.conf" "$HOME/.config/electron-flags.conf.bak"
 ln -s "$HOME/dotfiles/.config/electron-flags.conf" "$HOME/.config/electron-flags.conf"
 mv "$HOME/.config/vesktop-flags.conf" "$HOME/.config/vesktop-flags.conf.bak"
 ln -s "$HOME/dotfiles/.config/vesktop-flags.conf" "$HOME/.config/vesktop-flags.conf"
+
 mv "$HOME/.config/anyrun/config.ron" "$HOME/.config/anyrun/config.ron.bak"
 ln -s "$HOME/dotfiles/.config/anyrun/config.ron" "$HOME/.config/anyrun/config.ron"
+
 mv "$HOME/.config/xdg-desktop-portal/hyprland-portals.conf" "$HOME/.config/xdg-desktop-portal/hyprland-portals.conf.bak"
 ln -s "$HOME/dotfiles/.config/xdg-desktop-portal/hyprland-portals.conf" "$HOME/.config/xdg-desktop-portal/hyprland-portals.conf"
+
--- scripts/wallpaper.py
+++ scripts/wallpaper.py
@@ -8,15 +8,6 @@ import vdf
 import subprocess
 from pathlib import Path
 
-if __name__ != "__main__":
-    sys.exit(1)
-
-args = sys.argv[1:]
-if (len(args)):
-    DEBUG_WALLPAPER = args[0][args[0].rfind("=")+1:] # url or path, path must not contain = obv...
-else:
-    DEBUG_WALLPAPER = False
-
 # find the wallpaper engine config file from steam.
 wallpaper_engine_app_id = 431960
 
@@ -27,96 +18,121 @@ steam_content_paths = [
     "snap/steam/common/.local/share/Steam/steamapps"
 ]
 
-home = os.environ['HOME']
+def find_library_folders():
+    home = os.environ['HOME']
+    for path in steam_content_paths:
+        if os.path.exists(f"{home}/{path}/"):
+            config_path = f"{home}/{path}/"
+            break
+    
+    if not config_path:
+        return None
+    
+    with open(f"{config_path}libraryfolders.vdf", "r") as f:
+        library_folders = vdf.load(f)
+    
+    return [value["path"] for value in library_folders["libraryfolders"].values()]
 
-for path in steam_content_paths:
-    if os.path.exists(f"{home}/{path}/"):
-        config_path = f"{home}/{path}/"
-        break
 
-# if the config file is not found, exit the program.
-if not config_path:
-    print("Steam not found.")
-    sys.exit(1)
+def find_wallpaper_engine_config(library_folders):
+    for folder in library_folders:
+        appmanifest_path = f"{folder}/steamapps/appmanifest_{wallpaper_engine_app_id}.acf"
+        if os.path.exists(appmanifest_path):
+            break
+    
+    if not os.path.exists(appmanifest_path):
+        return None
+    
+    with open(appmanifest_path, "r") as f:
+        appmanifest = vdf.load(f)
+    
+    installdir = appmanifest["AppState"]["installdir"]
+    return f"{folder}/steamapps/common/{installdir}/config.json"
 
-# read the library folders vdf file
-with open(f"{config_path}libraryfolders.vdf", "r") as f:
-    library_folders = vdf.load(f)
+def get_roots():
+    monitors = subprocess.run(["wlr-randr", "--json"], capture_output=True).stdout.decode("utf-8")
+    monitors = json.loads(monitors)
+    return sum([["--screen-root", monitor["name"]] for monitor in monitors], [])
 
-folders = [value["path"] for value in library_folders["libraryfolders"].values()]
+def get_playlist(config_path):
+    with open(config_path, "r") as f:
+        config = json.load(f)
+    lastSelectedMonitor = config["steamuser"]["general"]["browser"]["lastselectedmonitor"]
+    playlists = config["steamuser"]["general"]["wallpaperconfig"]["selectedwallpapers"]
 
-# find the wallpaper engine appmanifest file
-for folder in folders:
-    appmanifest_path = f"{folder}/steamapps/appmanifest_{wallpaper_engine_app_id}.acf"
-    if os.path.exists(appmanifest_path):
-        break
+    if lastSelectedMonitor not in playlists:
+        return None, None
+    
+    mon = playlists[lastSelectedMonitor]
 
-if not os.path.exists(appmanifest_path):
-    print("Wallpaper Engine not found.")
-    sys.exit(1)
+    if "playlist" not in mon:
+        return None, None
 
-with open(appmanifest_path, "r") as f:
-    appmanifest = vdf.load(f)
+    items = mon["playlist"]["items"]
+    switch_time = mon["playlist"]["settings"]["delay"] * 60
+    return items, switch_time
 
-# open the wallpaper engine config json from path in appmanifest
-installdir = appmanifest["AppState"]["installdir"]
-config_path = f"{folder}/steamapps/common/{installdir}/config.json"
 
-with open(config_path, "r") as f:
-    config = json.load(f)
-
-lastSelectedMonitor = config["steamuser"]["general"]["browser"]["lastselectedmonitor"]
-playlists = config["steamuser"]["general"]["wallpaperconfig"]["selectedwallpapers"]
-
-if lastSelectedMonitor not in playlists:
-    print("No playlist found for the last selected monitor.")
-    sys.exit(1)
-
-# get monitor id's from wlr-randr
-monitors = subprocess.run(["wlr-randr", "--json"], capture_output=True).stdout.decode("utf-8")
-monitors = json.loads(monitors)
-
-items = playlists[lastSelectedMonitor]["playlist"]["items"]
-switch_time = playlists[lastSelectedMonitor]["playlist"]["settings"]["delay"] * 60
-roots = sum([["--screen-root", monitor["name"]] for monitor in monitors], [])
-
-# select random from playlist for monitor
-while True:
-    if DEBUG_WALLPAPER:
-        if DEBUG_WALLPAPER.startswith("Z:"):
-            selected = DEBUG_WALLPAPER
-        else:
-            selected = "  " + DEBUG_WALLPAPER
-    else:
-        selected = random.choice(items)
-
-    # get path to wallpaper
-    wallpaper_path = str(Path(selected[2:]).parent)
-
+def render_wallpaper(wallpaper, timeout, quiet):
+    roots = get_roots()
     args = [
         "linux-wallpaperengine", 
         *roots,
         "--fps=25",
         "--silent",
         "--scaling=default", 
-        wallpaper_path
+        wallpaper
     ]
-    if DEBUG_WALLPAPER:
-        monitorProcess = subprocess.Popen(
-            args
-        )
-    else:
+    if quiet:
         monitorProcess = subprocess.Popen(
             args,
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL
         )
+    else:
+        monitorProcess = subprocess.Popen(
+            args
+        )
     try:
-        monitorProcess.wait(timeout=switch_time)
+        monitorProcess.wait(timeout=timeout)
     except subprocess.TimeoutExpired:
         pass
     finally:
         monitorProcess.kill()
+    return
+
+
+# select random from playlist for monitor
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if (len(args)):
+        DEBUG_WALLPAPER = args[0][args[0].rfind("=")+1:] # url or path, path must not contain = obv...
+    else:
+        DEBUG_WALLPAPER = False
     
-    if DEBUG_WALLPAPER:
-        break
+    library_folders = find_library_folders()
+    if library_folders is None:
+        print("No library folders found.")
+        sys.exit(1)
+
+    config_path = find_wallpaper_engine_config(library_folders)
+    if config_path is None:
+        print("No wallpaper engine config found.")
+        sys.exit(1)
+
+    while True:
+        items, switch_time = get_playlist(config_path)
+
+        if DEBUG_WALLPAPER:
+            if DEBUG_WALLPAPER.startswith("Z:"):
+                selected = DEBUG_WALLPAPER
+            else:
+                selected = "  " + DEBUG_WALLPAPER
+            # get path to wallpaper
+            wallpaper_path = selected[2:]
+        else:
+            selected = random.choice(items)
+            # get path to wallpaper
+            wallpaper_path = str(Path(selected[2:]).parent)
+
+        render_wallpaper(wallpaper_path, switch_time, not DEBUG_WALLPAPER)
