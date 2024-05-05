cdb5510321d0a358a95014af710a81d69de2219b
Wagyourtail <wagyourtail@wagyourtail.xyz>
2024-05-05 00:58:43 -0500

    just check exists

--- scripts/clear_install_bak.sh
+++ scripts/clear_install_bak.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for file in $(cat "$HOME/dotfiles/tracked.txt") ; do
-    if [ -f "$HOME/$file.bak" ] ; then
+    if [ -e "$HOME/$file.bak" ] ; then
         rm "$HOME/$file.bak"
     fi
 done
